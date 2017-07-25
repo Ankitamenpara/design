9abcced4ceac028b8a5f62a4188ad32d57d2368b
ankita <ankita@improwised.com>
2017-07-25 13:54:15 +0530

    login/logout

--- app.js
+++ app.js
@@ -4,26 +4,34 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongodb = require('mongodb');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('client-sessions');
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressSession({secret: 'max' , saveUninitialized: false, resave: false}));
 app.use('/', index);
 app.use('/users', users);
+app.use(session({
+	cookieName: 'session',
+	secret: 'max',
+	duration : 30* 60 *1000,
+	activeDuration: 5*60*1000,
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,102 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var url = 'mongodb://localhost:27017/users';
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('client-sessions');
 
+mongo.connect(url, function(err, db){
+	if(err) throw err
+		db.createCollection('users', function(err,collection){
+
+		})
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home',{success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+	mongo.connect(url, function(err, db){
+		if(err) throw err
+			
+			db.collection('users').find({first_name : req.body.id , password : req.body.password}).toArray(function(err, result){
+				
+				if(result.length !== 0) {
+					req.session.result = result;
+					
+  					res.redirect('/dashboard');
+  				}else{
+  					res.redirect('/login');
+  				}
+			});
+	});
+});
+
+router.post('/register', function(req, res, next) {
+	
+	req.session.success = true;
+		
+	var item = {
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		email: req.body.email,
+		password: req.body.password,
+	};
+	mongo.connect(url, function(err, db){
+		if(err) throw err
+
+			db.collection('users').insertOne(item, function(err, result){
+		
+				res.redirect('/dashboard');
+			
+				db.close();
+
+			})
+		
+	});
+  
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+
+router.get('/dashboard', function(req, res, next) {
+	
+	if(req.session && req.session.result){
+	
+		mongo.connect(url, function(err, db){
+		if(err) throw err
+		db.collection('users').find({first_name : req.session.result[0].first_name}).toArray(function(err, result){
+			
+			if(!result){
+				req.session.reset();
+				res.render('login');
+
+			}else{
+				res.locals.result = result;
+				res.render('dashboard');
+			}
+
+
+		});
+})
+	}else{
+		res.render('login');
+	}
+
+});
+ 
+router.get('/logout', function(req, res, next) {
+	req.session.reset();
+  	res.redirect('/');
+});
 module.exports = router;
